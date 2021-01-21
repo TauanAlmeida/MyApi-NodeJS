7a07d68fe7e47c700fcb758105e00fe54904044d
Tauan Almeida <tauan_almeida@hotmail.com>
2021-01-20 21:02:02 -0300

    fix endpoints url

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const routes = express.Router()
-const User = require('./app/controllers/User')
-const { 
-    validateUsers, 
-    validateAuth, 
-    validateLostPassword, 
-    validateResetPassword
-} = require('./app/validators/validators')
+const express = require("express");
+const routes = express.Router();
+const User = require("./app/controllers/User");
+const {
+  validateUsers,
+  validateAuth,
+  validateLostPassword,
+  validateResetPassword,
+} = require("./app/validators/validators");
 
 //Rotas para Usuários
-routes.get('/users', User.index)
-routes.post('/register', validateUsers, User.store)
-routes.get('/users/:id', User.show)
-routes.put('/users/:id', User.update)
-routes.delete('/users/:id', User.destroy)
+routes.get("/users", User.index);
+routes.post("/user", validateUsers, User.store);
+routes.get("/user/:id", User.show);
+routes.put("/user/:id", User.update);
+routes.delete("/user/:id", User.destroy);
 
-routes.post('/lostPassword', validateLostPassword, User.lostPassword)
-routes.post('/resetPassword', validateResetPassword, User.resetPassword)
+routes.post("/lostPassword", validateLostPassword, User.lostPassword);
+routes.post("/resetPassword", validateResetPassword, User.resetPassword);
 
-routes.post('/getMail', User.userHasMail)
+routes.post("/getMail", User.userHasMail);
 
-routes.post('/getUserByEmail', User.getUserbyEmail)
-routes.post('/getUserByToken', User.getUserByToken)
+routes.post("/getUserByEmail", User.getUserbyEmail);
+routes.post("/getUserByToken", User.getUserByToken);
 
 //Rota de autenticação
-routes.post('/auth', validateAuth, User.authenticate)
+routes.post("/auth", validateAuth, User.authenticate);
 
-
-module.exports = routes
+module.exports = routes;
